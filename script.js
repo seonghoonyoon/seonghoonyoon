02438af326cebf9c2192b15cf90da2204da951b5
seonghoonyoon <yoonseonghoon99@gmail.com>
2024-10-07 18:09:34 -0400

    doesn't work yet

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ for (i = 0; i < toggler.length; i++) {
         this.classList.toggle("caret-down");
     });
 }
+
+var toggler2 = document.getElementsByClassName("click");
+var togglee = document.getElementsByClassName("item")
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+    toggler2[i].addEventListener("click", function() {
+        togglee[i].parentElement.querySelector(".nested").classList.toggle("active");
+        togglee[i].classList.toggle("caret-down");
+    });
+}
